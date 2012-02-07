3416efbab3f659aedb05378a60df37cbe24c40a1
Atalanta Systems Engineering <support@atalanta-systems.com>
2012-02-07 19:51:28 +0000

    Fix

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,3 @@ license          "All rights reserved"
 description      "Installs/Configures users"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "2.0.4"
-<<<<<<< HEAD
-
-=======
->>>>>>> 77060aeb3ee45c18e606fb79d720bef596e7d577
