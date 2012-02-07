77060aeb3ee45c18e606fb79d720bef596e7d577
Atalanta Systems Engineering <support@atalanta-systems.com>
2012-02-07 19:44:49 +0000

    Attributes should be set to shell name not path

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,4 @@ default['users']['sharing_comment'] = 'Atalanta Systems Engineering'
 default['users']['sharing_shell']   = '/bin/bash'
 default['users']['sharing_tools']   = ['git', 'emacs', 'screen', 'mg', 'irssi']
 default['users']['home_base'] = '/export/home'
-default['users']['shell'] = '/usr/bin/bash'
+default['users']['shell'] = 'bash'
