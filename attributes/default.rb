0e9e5f0a97ffb6063d7fabafa1d82680debd3928
Konstantin Lysenko <konstantin@atalanta-systems.com>
2012-10-21 23:38:53 +0300

    Remove duplicate attributes in default and sysadmin attribute files. I think it was a reason of generating of wrong resource.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,5 @@ default['users']['sharing_user']    = 'atalanta'
 default['users']['sharing_comment'] = 'Atalanta Systems Engineering'
 default['users']['sharing_shell']   = '/bin/bash'
 default['users']['sharing_tools']   = ['git', 'emacs', 'screen', 'mg', 'irssi']
-default['users']['home_base'] = '/export/home'
-default['users']['shell'] = 'bash'
 default['users']['token_group'] = "atalanta"
+default['users']['shell'] = 'bash'
--- attributes/sysadmins.rb
+++ attributes/sysadmins.rb
@@ -1,7 +1,6 @@
 case platform
   when "solaris2"
     default['users']['home_base'] = '/export/home'
-    default['users']['shell'] = 'bash'
   else
     default['users']['home_base'] = '/home'
 end
